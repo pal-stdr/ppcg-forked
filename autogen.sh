bd12c1a4f1c02e010e21ea4dac859853ad2ab931
Pallab Sutradhar <pallab.sutradhar@upm.es>
2024-07-15 20:56:28 +0200

    cjson & osl add as git submodule

--- autogen.sh
+++ autogen.sh
@@ -5,4 +5,15 @@ fi
 if test -f pet/autogen.sh; then
 	(cd pet; ./autogen.sh)
 fi
+
+echo -e "\n*** Running autotools on osl ***"
+if test -f osl/autogen.sh; then
+	(cd osl; ./autogen.sh)
+fi
+
+echo -e "\n*** Running autotools on cJSON ***"
+if test -f cjson/autogen.sh; then
+	(cd cjson; ./autogen.sh)
+fi
+
 autoreconf -i
